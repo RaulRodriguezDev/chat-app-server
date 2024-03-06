1c6467d989a7d774e41d4b3ec102637de8c701ca
Rodriguez, Raul Ivan <raul.rodriguez.dev@gmail.com>
2024-03-06 16:39:23 -0600

    adding schemas

---
+++ models/message.js
@@ -0,0 +1,28 @@
+import { Schema } from "mongoose";
+
+const MessageSchema = new Schema({
+    from: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    to: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    timestamps: true
+})
+
+MessageSchema.method('toJSON', function(){
+    const { __v, ...object } = this.toObject()
+    return object
+})
+
+const message = model('Message', MessageSchema)
+
+export default message
---
+++ models/user.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    online: {
+        type: Boolean,
+        default: false
+    }
+})
+
+UserSchema.method('toJSON', function(){
+    const { __v, __id, password, ...object } = this.toObject()
+    object.uid = _id
+    return object
+})
+
+const user = model('User', UserSchema)
+
+export default user
