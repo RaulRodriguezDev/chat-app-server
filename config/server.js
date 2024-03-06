12f7474a108ae35f5fd0349d94c2d647c9c73731
Rodriguez, Raul Ivan <raul.rodriguez.dev@gmail.com>
2024-03-06 15:57:56 -0600

    Connect the database to the server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import Http from "http"
 import dotenv from "dotenv"
+import { dbConnection } from "./db.js"
 
 dotenv.config({ path: ".env" })
 class Server {
@@ -8,6 +9,8 @@ class Server {
         this.app = express()
         this.port = process.env.PORT
         this.httpServer = Http.createServer(this.app)
+
+        dbConnection()
     }
 
     init() {
